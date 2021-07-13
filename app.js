a61d45070ae9063731b226497dbf0422904682cd
Aayush Bisen <aayushbisen@protonmail.com>
2021-07-13 14:26:22 +0530

    Add user post route

--- app.js
+++ app.js
@@ -7,8 +7,16 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "This is home page." });
+  res.status(200).json({ message: "Welcome!!" });
 });
+
+app.post('/user',(req, res)=>{
+  const {name} = req.body;
+  if(!name){
+    return res.status(400).json({message: "Name should not be empty."})
+  }
+  res.status(200).json({message: "Hello "+name+"! Welcome to this website."})
+})
 
 app.listen(PORT, () => {
   console.log("The server is up BOY!");
